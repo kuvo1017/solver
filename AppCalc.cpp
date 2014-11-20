709ee50e1f7612b84b04c5a3686c0aa0fa0bec0c
Bob <kubo.cota1017@gmail.com>
2014-11-20 17:49:46 +0900

    3倍の時間のシミュレーションを行うように設定

--- AppCalc.cpp
+++ AppCalc.cpp
@@ -144,7 +144,7 @@ int AppCalc::batchRun()
 
     cout << "*** Run advmates-calc: max time=" << maxTime
          << " (dt=" << TimeManager::unit() << ") ***" << endl;
-    _simulator->run(maxTime/1000000);
+    _simulator->run(maxTime*3);
 
     if (GVManager::getFlag("FLAG_OUTPUT_TRIP_INFO"))
     {
