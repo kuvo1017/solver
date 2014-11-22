ec2c45b49923d22358d218c70e5d205e63d10eb7
Bob <kubo.cota1017@gmail.com>
2014-11-22 18:52:44 +0900

    error fixed

---
+++ file.rb
@@ -0,0 +1,31 @@
+# coding:utf-8 
+require 'csv'
+
+csv = CSV.open("_result_whole_" + ARGV[0]+ ".csv" ,"w") 
+#outfile = File.open('_result_whole.csv', 'a')
+#CSV::Writer.generate(outfile) do |writer|
+#csv = CSV.open("_result_whole_" + $ARGV[0] + ".csv","w")
+#csv << ["計算時間","シミュレーション内時間","小型車発生台数","大型車発生台数","事故数","エラー率"]
+rates = ["0.001","0.002","0.003","0.005","0.008","0.010","0.020","0.030","0.050", "0.1"]
+rates.each do |r|
+  file = open("./_stat_accident" + r + "000 .txt")
+  count = 0
+  file.each do |line|
+    count+=1
+  end
+  nLine = count
+  p "==============="
+  p "nLine:" + nLine.to_s
+  p "==============="   
+  count = 0
+
+  file2 = open("./_stat_accident" + r + "000 .txt") 
+  file2.each do |line|
+    count+=1
+    p count
+    if count == nLine-1 
+      new_line = [line.gsub(",\n","") + "," + r]
+      csv << new_line 
+    end
+  end
+end
---
+++ headscript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ for i in {1..3}
+ do
+   for rate in  0.001 0.002 0.003 0.005 0.008 0.01 0.02 0.03 0.05 0.1
+   do
+	./advmates-calc -d $rate
+   done
+   ruby ./file.rb $i
+ done
