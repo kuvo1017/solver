e9848871ca3799a5929befd2442fe3178040aeae
kubo <kickmr@yahoo.co.jp>
2015-01-02 14:36:15 +0900

    for conflict

--- AppCalc.cpp
+++ AppCalc.cpp
@@ -149,7 +149,7 @@ int AppCalc::batchRun()
 
     cout << "*** Run advmates-calc: max time=" << maxTime
          << " (dt=" << TimeManager::unit() << ") ***" << endl;
-    _simulator->run(maxTime*3);
+    _simulator->run(maxTime);
 
     if (GVManager::getFlag("FLAG_OUTPUT_TRIP_INFO"))
     {
