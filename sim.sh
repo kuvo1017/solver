a585ca6228936b5e85724bc4f595f184d39e79fc
Bob <kubo.cota1017@gmail.com>
2015-01-04 09:50:45 +0900

    sim.sh改良

--- sim.sh
+++ sim.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-for i in "0.001" "0.002" "0.003" "0.01" "0.05" "0.1" "0.3" "0.5"
+for n in "res1" "res2" "res3"  
 do
- ./advmates-calc -d $i
+  for i in "0.001" "0.002" "0.003" "0.01" "0.05" "0.1" "0.3" "0.5"
+  do
+    ./advmates-calc -d $i
+  done
+  mkdir $n
+  cp _stat* _accident* _error* ./$n
 done
